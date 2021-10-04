57cb5b220a07003dcbdf1332338cb0cd7c9ac31f
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-04 16:52:28 +0600

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cart = () => {
     return (
         <div>
-
+            <h2 className="text-center fs-1 my-5 fw-bold">Cart is Not Ready</h2>
         </div>
     );
 };
